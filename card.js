a413f0570ef00a872a155606a63ec1dc4241fc39
Deepakfrost <42265988+Deepakfrost@users.noreply.github.com>
2022-11-18 19:36:20 +0530

    Create card.js

---
+++ card.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div className="card">
+        <h1>this is inside a card</h1>
+      </div>
+    </div>
+  );
+}
